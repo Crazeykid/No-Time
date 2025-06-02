84993601e6d65fde9ecf3a7270574cb99a31621a
Crazeykid <huntermtd@gmail.com>
2025-06-01 23:22:32 -0700

    Add files via upload

--- No Time/character_body_2d.gd
+++ No Time/character_body_2d.gd
@@ -3,5 +3,5 @@ extends CharacterBody2D
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("Left", "Right", "Up", "Down")
-	velocity = direction * 500
+	velocity = direction * 300
 	move_and_slide()
