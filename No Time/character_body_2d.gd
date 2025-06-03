76fc63618f43686c124bbbecb4fa16be2de806db
Crazeykid <huntermtd@gmail.com>
2025-06-03 10:09:11 -0700

    latest

--- No Time/character_body_2d.gd
+++
@@ -1,7 +0,0 @@
-extends CharacterBody2D
-
-
-func _physics_process(delta: float) -> void:
-	var direction = Input.get_vector("Left", "Right", "Up", "Down")
-	velocity = direction * 300
-	move_and_slide()
