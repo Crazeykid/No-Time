c639a464271a13cd224ef96cf173e87f8a111336
Crazeykid <huntermtd@gmail.com>
2025-06-06 00:46:40 -0700

    fixes

--- No Time/SCRIPTS/knight.gd
+++ No Time/SCRIPTS/knight.gd
@@ -14,7 +14,7 @@ var dash_speed := 500.0       # Speed during dash
 var normal_speed := 60.0
 
 var is_waiting := false       # True when knight is stopped and waiting
-var wait_time := 0.5          # Time to wait before dashing
+var wait_time := 0.50         # Time to wait before dashing
 var wait_timer := 0.0         # Timer counting how long knight has waited
 var is_dashing := false       # True while knight is dashing
 var dash_direction := Vector2.ZERO
@@ -29,9 +29,9 @@ func _physics_process(delta: float) -> void:
 		direction = dash_direction
 		velocity = dash_direction * dash_speed
 		wait_timer += delta
-		if wait_timer >= 0.5:  # DASH LENGTH
+		if wait_timer >= 0.41:  # DASH LENGTH
 			is_dashing = false
-			wait_timer = 0.0
+			wait_timer = 0
 	elif is_waiting:
 		direction = Vector2.ZERO
 		velocity = Vector2.ZERO
